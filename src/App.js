20da2cde20677bed4d83919b24dcaf7393727546
Eugene <ddd.eugene@gmail.com>
2020-12-02 20:45:05 +0800

    with header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
 //pages
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
+//components
+import Header from './components/header/header.component';
+
 
 
 function App() {
   return (
     <div >
-      
-        <Route exact path={'/'} component={HomePage}/>
-        <Route exact path={'/shop'} component={ShopPage}/>
+        <Header />
+
+        <Switch >
+          <Route exact path={'/'} component={HomePage}/>
+          <Route exact path={'/shop'} component={ShopPage}/>
+        </Switch>
+        
       
     </div>
   );
