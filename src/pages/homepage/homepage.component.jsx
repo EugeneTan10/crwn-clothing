7ca7e109649c2530d1a76f6eb15e6230d4e89471
Eugene <ddd.eugene@gmail.com>
2020-12-02 12:23:20 +0800

    homepage update

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// scss
+import './homepage.style.scss';
+
+// components
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
